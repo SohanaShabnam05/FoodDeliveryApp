c939214ce19354a179436d2be8a5763fc1d93c72
sohana <sshabnamzsf@gmail.com>
2022-09-12 11:36:04 +0530

    new

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -3,45 +3,46 @@ import { useRef, useState } from 'react';
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
-const MealItemForm = props => {
-    const amountInputRef = useRef();
+const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
 
-    const [amountIsValid, setAmountIsValid] = useState('');
+  const submitHandler = (event) => {
+    event.preventDefault();
 
-    const submitHandler = event => {
-        event.preventDefault();
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
 
-        const enteredAmount = amountInputRef.current.value;
-        const enteredAmountNumber = +enteredAmount;
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
 
-        if(enteredAmount.trim().length === 0 ||
-         enteredAmountNumber < 1 || 
-         enteredAmountNumber > 5)
-         {
-            setAmountIsValid(false);
-            return;
-         }  
+    props.onAddToCart(enteredAmountNumber);
+  };
 
-         props.onAddToCart(enteredAmountNumber);
-    };
-
-    return (
-     <form className={classes.form} onSubmit={submitHandler}>
-        <Input 
+  return (
+    <form className={classes.form} onSubmit={submitHandler}>
+      <Input
         ref={amountInputRef}
-        label="Amount :" 
+        label='Amount'
         input={{
-            id: 'amount',
-            type: 'number',
-            min: '1',
-            max: '5',
-            step: '1',
-            defaultValue: '1'
-        }} />
-        <button>+ Add </button>
-        {!amountIsValid && <p>Please enter a valid amount (1-5)</p>}
+          id: 'amount_' + props.id,
+          type: 'number',
+          min: '1',
+          max: '5',
+          step: '1',
+          defaultValue: '1',
+        }}
+      />
+      <button>+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
     </form>
-    )
+  );
 };
 
 export default MealItemForm;
--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -3,24 +3,24 @@ import ReactDOM from 'react-dom';
 
 import classes from './Modal.module.css';
 
-const Backdrop = props => {
+const Backdrop = (props) => {
     return <div className={classes.backdrop} onClick={props.onClose}/>
-}
+};
 
-const ModalOverlay = props => {
+const ModalOverlay = (props) => {
     return (
     <div className={classes.modal}>
         <div className="classes.content">{props.children}</div>
     </div>
-    )
-}
+    );
+};
 
 const portalElement = document.getElementById('overlays');
 
-const Modal = props => {
+const Modal = (props) => {
     return <Fragment>
 
-    {ReactDOM.createPortal(<Backdrop onClose={props.onClose}/>, portalElement)}
+    {ReactDOM.createPortal(<Backdrop onClose={props.onClose} />, portalElement)}
     {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>,portalElement)}
     
     </Fragment>
